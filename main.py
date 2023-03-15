c6edf2bb1292d137379469be1f3480679c538d90
Marco Slabbert <60457007+pYcotic@users.noreply.github.com>
2023-03-15 08:52:11 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from Classes import recipe, create_recipe
+from Classes import create_recipe, recipe
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+kivy.require('2.1.0')
 
 
 cr = create_recipe.create_recipe
 cr(recipe.Recipe)
 
 
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
 if __name__ == '__main__':
-    # print(create_recipe(recipe))
-    pass
+    MyApp().run()
 
