6bb2adbfd7aef67e5c15c5ebd71d8eab343f52f1
Marco Slabbert <60457007+pYcotic@users.noreply.github.com>
2023-03-15 08:16:36 +0700

    Initial commit

--- Classes/shopping_list.py
+++
@@ -1,9 +0,0 @@
-# this class takes a list of recipes and identifies which ingredients are necessary to make all the recipes
-class ShoppingList:
-    def __init__(self, recipes: list):
-        self.recipes = recipes
-
-    def createShoppingList(self):
-        items = list(tuple(self.recipes))
-        return items
-
